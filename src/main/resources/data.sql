cd0e3bd3b1f50ae803f7d83c11f017c7afac9105
MinhQuan <minh.quan040501@gmail.com>
2026-02-25 16:28:47 +0700

    Add script to seed database during initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO trader (trader_name, created_at) VALUES ('trader', CURRENT_TIMESTAMP);
+
+INSERT INTO wallet (asset, balance, trader_id, created_at) VALUES ('USDT', '50000', 1, CURRENT_TIMESTAMP);
